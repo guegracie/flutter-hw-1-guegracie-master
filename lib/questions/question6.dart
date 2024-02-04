f45eabb0d306056e5770f113b6f69a07ef35123e
guegracie <95649024+guegracie@users.noreply.github.com>
2024-02-04 13:23:05 -0600

    first and final commit

--- lib/questions/question6.dart
+++ lib/questions/question6.dart
@@ -21,7 +21,7 @@ class Question6 extends StatelessWidget {
           body: Center(
             // ADD IMAGE AS A CHILD OF CENTER
              child: Image.network('https://i.insider.com/5e32f2a324306a19834af322?width=1800&format=jpeg&auto=webp'),
-            
+            //done
             
           )),
           
